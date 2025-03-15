e1b9d23859a2fba5d1fbd6a6133a6a0022fe4eed
EngHussam23 <engraggad60@gmail.com>
2025-03-15 20:47:37 +0300

    first commit

---
+++ src/Layouts/topnav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const topnav = () => {
+  return (
+    <section>
+      <img src="" alt="School Logo" width={32} height={32} />
+      <div>
+        <a href="">Text</a>
+        <a href="">Text</a>
+        <a href="">Text</a>
+      </div>
+    </section>
+  );
+};
+
+export default topnav;
